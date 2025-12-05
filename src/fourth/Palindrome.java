669683d04d4a9da7f89542e3fb53fbeb419fe649
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-12-05 14:18:23 +0300

    refactor: выполнить задание 4

--- src/fourth/Palindrome.java
+++ src/fourth/Palindrome.java
@@ -3,15 +3,10 @@ package fourth;
 public class Palindrome {
     public boolean isPalindromeLine(String str) {
         String lowerCaseStr = str.toLowerCase().trim();
-
         StringBuilder processedString = new StringBuilder(lowerCaseStr);
-        int i = 0;
-        while (i < processedString.length()) {
-            if (processedString.charAt(i) == ' ') {
-                processedString.deleteCharAt(i);
-            } else {
-                i++;
-            }
+
+        while (processedString.indexOf(" ") != -1) {
+            processedString.deleteCharAt(processedString.indexOf(" "));
         }
 
         StringBuilder reversedProcessedString = new StringBuilder(processedString);
