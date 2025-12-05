e02d86a6ac6b07d900b9281f09165fc03f735437
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-12-05 13:56:11 +0300

    feat: выполнить задание 1

---
+++ src/PoemFixer.java
@@ -0,0 +1,28 @@
+public class PoemFixer {
+    public String[] readPoem() {
+        return new String[]{
+                "   Это кто там ложку «ложит»?",
+                "",
+                "   ",
+                "Знай, такого быть не может!",
+                "  Ложку мы на стол кладём,  ",
+                "",
+                "А тебя – к обеду ждём."
+        };
+    }
+
+    public String fixPoem(String[] poem) {
+        StringBuilder string = new StringBuilder(); // допишите код
+        for (String line: poem) {
+            if (!line.isBlank()) string.append(line.trim());
+        }
+        return string.toString();
+    }
+
+    public static void main(String[] args) {
+        PoemFixer poemFixer = new PoemFixer();
+        String[] poem = poemFixer.readPoem();
+        String poemAsString = poemFixer.fixPoem(poem);
+        System.out.println(poemAsString);
+    }
+}
